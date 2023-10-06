2c74b405291bdca6675e09b35e0e1c88291b77d6
zuracode <110834226+zuracode@users.noreply.github.com>
2023-10-06 22:22:40 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,66 @@
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+const vowels = ["a", "e", "i", "o", "u"];
 
+const countVowel = (word) => {
+  return word.split("").reduce((acc, curr) => {
+    if (vowels.includes(curr)) {
+      return (acc += 1);
+    }
+    return acc;
+  }, 0);
+};
+
+const findLongestWord = (sentence) => {
+  const lowercasedSentense = sentence.toLowerCase();
+
+  const wordsList = lowercasedSentense.split(" ").map((word) => {
+    if (!alphabet.includes(word.charAt(word.length - 1))) {
+      return word.slice(0, -1);
+    }
+    return word;
+  });
+
+  let maxLenWord = "";
+
+  wordsList.forEach((word) => {
+    if (word.length > maxLenWord.length) {
+      maxLenWord = word;
+    } else if (word.length === maxLenWord.length) {
+      const countVowelPrev = countVowel(maxLenWord);
+      const countVowelCurr = countVowel(word);
+
+      if (countVowelCurr > countVowelPrev) {
+        maxLenWord = word;
+      }
+    }
+  });
+
+  return maxLenWord;
+};
